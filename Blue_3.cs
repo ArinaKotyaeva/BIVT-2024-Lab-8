b1bf5450f61eb5a3db0adcac3a274267b06b4397
kotyaeva_a <m2405866@edu.misis.ru>
2025-04-22 11:54:55 +0300

    Update Blue_3.cs

--- Blue_3.cs
+++ Blue_3.cs
@@ -7,7 +7,17 @@ namespace Lab_8
     public class Blue_3 : Blue
     {
         private (char, double)[] _output;
-        public (char, double)[] Output => _output;
+        public (char, double)[] Output
+        {
+            get
+            {
+                if (_output == null) return null;
+
+                (char, double)[] copy = new (char, double)[_output.Length];
+                Array.Copy(_output, copy, _output.Length);
+                return copy;
+            }
+        };
 
         public Blue_3(string input) : base(input)
         {
@@ -18,87 +28,61 @@ namespace Lab_8
         {
             if (string.IsNullOrWhiteSpace(Input))
             {
-                _output = new (char, double)[0];
+                _output = null;
                 return;
             }
 
-            string[] words = GetFilteredWords(Input);
-            var letterCounts = new System.Collections.Generic.Dictionary<char, int>();
-            int totalWords = 0;
+            char[] signs = { ' ', '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/' };
+            string words = Input.Split(signs, StringSplitOptions.RemoveEmptyEntries); ;
+            int count_l = new int[char.MaxValue];
+            int count_w = 0;
 
             foreach (string word in words)
             {
                 if (word.Length == 0) continue;
 
-                char firstChar = char.ToLower(GetFirstLetter(word));
-                if (firstChar == '\0') continue;
+                char first = char.ToLower(word[0]);
+                if (char.IsLetter(first))
+                {
+                    count_l[first]++;
+                    count_w++;
+                }
 
-                totalWords++;
-                if (letterCounts.ContainsKey(firstChar))
-                    letterCounts[firstChar]++;
-                else
-                    letterCounts[firstChar] = 1;
+            }
+            int uni = 0;
+            for (int i = 0; i < count_l.Length; i++)
+            {
+                if (count_l[i] > 0) uni++;
             }
 
-            _output = letterCounts
-                .Select(pair => (pair.Key, totalWords > 0 ? Math.Round(pair.Value * 100.0 / totalWords, 4) : 0))
-                .OrderByDescending(x => x.Item2)
-                .ThenBy(x => x.Item1)
-                .ToArray();
-        }
+            _output = new (char, double)[uni];
+            int ind = 0;
 
-        private string[] GetFilteredWords(string text)
-        {
-            var result = new System.Collections.Generic.List<string>();
-            var currentWord = new StringBuilder();
-
-            foreach (char c in text)
+            for (int i = 0; i < count_l.Length; i++)
             {
-                if (char.IsLetter(c) || c == '\'' || c == '-')
+                if (count_l[i] > 0)
                 {
-                    currentWord.Append(c);
-                }
-                else if (currentWord.Length > 0)
-                {
-                    result.Add(currentWord.ToString());
-                    currentWord.Clear();
+                    double res = Math.Round(count_l[i] * 100.0 / count_w, 4);
+                    _output[ind++] = ((char)i, res);
                 }
             }
 
-            if (currentWord.Length > 0)
+            Array.Sort(_output, (x, y) =>
             {
-                result.Add(currentWord.ToString());
-            }
-
-            return result.ToArray();
-        }
-
-        private char GetFirstLetter(string word)
-        {
-            foreach (char c in word)
-            {
-                if (char.IsLetter(c))
-                    return c;
-            }
-            return '\0';
+                int cmp = y.Item2.CompareTo(x.Item2);
+                return cmp != 0 ? cmp : x.Item1.CompareTo(y.Item1);
+            });
         }
 
         public override string ToString()
         {
-            if (_output == null || _output.Length == 0)
-                return string.Empty;
-
-            var result = new StringBuilder();
+            string result = "";
             for (int i = 0; i < _output.Length; i++)
             {
-                result.Append(_output[i].Item1);
-                result.Append(" - ");
-                result.Append(_output[i].Item2.ToString("F4"));
-                if (i < _output.Length - 1)
-                    result.AppendLine();
+                result += $"{_output[i].letter} - {_output[i].percent:f4}";
+                if (i != _output.Length - 1) result += "\n";
             }
-
-            return result.ToString();
+            return result;
         }
     }
 }
